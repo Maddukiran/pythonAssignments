3ca19f62f7d6a20ef561af42d13f4204892478e5
Pavan Manikanta Kiran Maddu <m.p.m.kiran@gmail.com>
2020-12-07 09:08:19 +0530

    overloading

--- Day4/classFive.py
+++ Day4/classFive.py
@@ -12,13 +12,27 @@ class Num:
     def __str__(self):
         return str(self.var)
 
-    def __add__(self, other):
+    def __add__(self, other): #overloading + operator
         return Num(self.var + other.var)
 
+    def __sub__(self, other): #overloading - operator
+        return Num(self.var + other.var)
+
+    def __lt__(self, other): #overloading > operator
+        return self.var > other.var
+
+    def __gt__(self, other): #overloading < operator
+        return self.var < other.var
+
 var1 = Num(100)
-var2 = Num('some sting here')
+
+var2 = Num('hello')
 
 var3 = var1 + var2
+
+print(var1 > var2)
+print(var1 < var2)
+
 print(f'var1--> {var1}')
 print(f'var2--> {var2}')
 print(f'var3--> {var3}')
